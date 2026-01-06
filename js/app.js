299590262735357ed919e99d25b6cb592c3d09f1
koromodental-debug <koromo.dental@gmail.com>
2026-01-06 22:19:01 +0900

    HTML padding 20px（純正と同じ）

--- js/app.js
+++ js/app.js
@@ -598,7 +598,7 @@
         .page {
           max-width: 100% !important;
           margin: 0 !important;
-          padding: 18px !important;
+          padding: 20px !important;
           padding-bottom: 150px !important;
         }
 
